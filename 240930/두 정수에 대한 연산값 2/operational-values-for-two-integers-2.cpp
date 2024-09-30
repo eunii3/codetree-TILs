cf7e577d5ef9e4821113a938715b08fe05aff995
Choi Gaeun <rose021223@chungbuk.ac.kr>
2024-09-30 16:57:59 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240930/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int a, b, small, big;
+    cin >> a >> b;
+
+    if(a<b){
+        small = a;
+        big = b;
+
+    }else{
+        small = b;
+        big = a;
+    }
+
+    cout << small + 10 << " " << big * 2;
+
+    return 0;
+}
